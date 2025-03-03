f813487eb4717e9c9758d2a769058b57606b799b
Rutger J.C. van der Knijff <84037385+TRutger@users.noreply.github.com>
2025-03-03 10:15:12 +0100

    Install charka UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// React
+import React from "react";
+import ReactDOM from "react-dom/client";
+// 3-Party Libraries
+import { ChakraProvider } from "@chakra-ui/react";
+// Own import statements
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
